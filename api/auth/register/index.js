fb46f2f597238ae0d4e0abe5c8839ebdd8876844
nmhung1234 <nmhung1234@hotmail.com>
2021-08-06 00:38:23 +0700

    validate register

--- api/auth/register/index.js
+++ api/auth/register/index.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./../../../models/user');
-const { db } = require('../../../repository')
+const { db } = require('../../../repository');
+const { generatePassword } = require('../../../Utils')
 
 router.post('/register', async function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
     const email = req.body.email;
-    const newUser = new User(email, password, username);
-    const findUser = await db.user.findOne({ "email": email });
-    if (findUser?._id) {
+
+    const genPass = generatePassword(password);
+    const newUser = new User(email, genPass.hassedPassword, username, genPass.salt);
+    
+    const findEmail = await db.user.findOne({ "email": email });
+    const findUsername = await db.user.findOne({ "username": username });
+    if (findEmail?.email) {
         res.send("Email đã tồn tại")
+    } else if (findUsername?.username) {
+        res.send("Username đã tồn tại")
     } else {
         db.user.insertOne(newUser)
         res.send(newUser);
