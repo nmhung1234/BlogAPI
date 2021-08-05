5316c87008f0d6b911558eaf5022ff8eab1f1a9a
nmhung1234 <nmhung1234@hotmail.com>
2021-08-06 00:39:50 +0700

    just create user

--- models/user/index.js
+++ models/user/index.js
@@ -1,26 +1,19 @@
 const crypto = require('crypto');
 const { generateTime } = require('./../../Utils')
 class User {
-    constructor(email, password, username) {
+    constructor(email, password, username, salt) {
         this.email = email;
         this.username = username;
         this.name = username;
-        this.sex = 0,
-        this.description = '',
-        this.favoriteTopic = null,
-        this.avatar = null,
-        this.type = 1,
-        this.password = this.generatePassword(password)["hassedPassword"].toString("base64");
-        this.salt = this.generatePassword(password)["privateKey"];
-        this.createdAt = generateTime();
+        this.sex = 0;
+        this.description = '';
+        this.favoriteTopic = null;
+        this.avatar = null;
+        this.type = 1;
+        this.password = password;
+        this.salt = salt;
+        this.createdAt = generateTime()
     }
-    generatePassword(password) {
-        const salt = crypto.randomBytes(128).toString("base64");
-        const hassedPassword = crypto.pbkdf2Sync(password, salt, 10000, 256, "sha512");
-        return {
-            "privateKey": salt,
-            "hassedPassword": hassedPassword
-        }
-    }
+
 }
 module.exports = User;
